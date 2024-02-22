6945c97f1621f73fc1ba636552f3dd033791a5c8
Audrey <156109823+AudreyVargas@users.noreply.github.com>
2024-02-22 10:48:27 -0600

    I was trying to make it so enemies will spawn after gamemode is chosen.

--- main.ts
+++ main.ts
@@ -1,1 +1,75 @@
-tiles.setCurrentTilemap(tilemap`level2`)
+function GameMode (EnemySpeed: number) {
+    tiles.setCurrentTilemap(tilemap`level2`)
+    enemiesList = [img`
+        . . . . . 3 3 b 3 3 d d 3 3 . . 
+        . . . . 3 1 1 d 3 d 1 1 1 1 3 . 
+        . . . 3 d 1 1 1 d 1 1 1 d 3 1 3 
+        . . 3 d d 1 1 1 d d 1 1 1 3 3 3 
+        . 3 1 1 d 1 1 1 1 d d 1 1 b . . 
+        . 3 1 1 1 d 1 1 1 1 1 d 1 1 3 . 
+        . b d 1 1 1 d 1 1 1 1 1 1 1 3 . 
+        . 4 b 1 1 1 1 d d 1 1 1 1 d 3 . 
+        . 4 4 d 1 1 1 1 1 1 d d d b b . 
+        . 4 d b d 1 1 1 1 1 1 1 1 3 . . 
+        4 d d 5 b d 1 1 1 1 1 1 1 3 . . 
+        4 5 d 5 5 b b d 1 1 1 1 d 3 . . 
+        4 5 5 d 5 5 d b b b d d 3 . . . 
+        4 5 5 5 d d d d 4 4 b 3 . . . . 
+        . 4 5 5 5 4 4 4 . . . . . . . . 
+        . . 4 4 4 . . . . . . . . . . . 
+        `, img`
+        . . . . . . . 6 . . . . . . . . 
+        . . . . . . 8 6 6 . . . 6 8 . . 
+        . . . e e e 8 8 6 6 . 6 7 8 . . 
+        . . e 2 2 2 2 e 8 6 6 7 6 . . . 
+        . e 2 2 4 4 2 7 7 7 7 7 8 6 . . 
+        . e 2 4 4 2 6 7 7 7 6 7 6 8 8 . 
+        e 2 4 5 2 2 6 7 7 6 2 7 7 6 . . 
+        e 2 4 4 2 2 6 7 6 2 2 6 7 7 6 . 
+        e 2 4 2 2 2 6 6 2 2 2 e 7 7 6 . 
+        e 2 4 2 2 4 2 2 2 4 2 2 e 7 6 . 
+        e 2 4 2 2 2 2 2 2 2 2 2 e c 6 . 
+        e 2 2 2 2 2 2 2 4 e 2 e e c . . 
+        e e 2 e 2 2 4 2 2 e e e c . . . 
+        e e e e 2 e 2 2 e e e c . . . . 
+        e e e 2 e e c e c c c . . . . . 
+        . c c c c c c c . . . . . . . . 
+        `, img`
+        . . . . . . . . . . . 6 6 6 6 6 
+        . . . . . . . . . 6 6 7 7 7 7 8 
+        . . . . . . 8 8 8 7 7 8 8 6 8 8 
+        . . e e e e c 6 6 8 8 . 8 7 8 . 
+        . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+        e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+        e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+        e 2 e e 2 2 2 2 e e e e c 6 8 . 
+        c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+        . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+        . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+        . . . e c c e c 2 2 2 2 2 2 2 e 
+        . . . . . . . c 2 e e 2 2 e 2 c 
+        . . . . . . . c e e e e e e 2 c 
+        . . . . . . . . c e 2 2 2 2 c . 
+        . . . . . . . . . c c c c c . . 
+        `]
+    SpawnTiles = tiles.getTilesByType(sprites.castle.tileDarkGrass1)
+    for (let index = 0; index < 4; index++) {
+        enemies = sprites.create(enemiesList._pickRandom(), SpriteKind.Enemy)
+        tiles.placeOnTile(enemies, SpawnTiles.removeAt(randint(0, SpawnTiles.length - 1)))
+    }
+    if (Gamemode < 6) {
+    	
+    } else {
+    	
+    }
+}
+let SpawnTiles: tiles.Location[] = []
+let enemiesList: Image[] = []
+let enemies: Sprite = null
+let Gamemode = 0
+Gamemode = game.askForNumber("Easy(<5) or hard(>6) mode.", 1)
+while (!(Gamemode < 6 && Gamemode > 6)) {
+    Gamemode = game.askForNumber("Easy(<5) or hard(>6) mode.", 1)
+}
+enemies.setVelocity(50, 50)
+GameMode(1)
